4805607f973be060c98791a70194d8eb088da89a
Gwen <1992379991@qq.com>
2025-02-24 19:26:05 +0800

    Update navigationJS.js

--- navigationJS.js
+++ navigationJS.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     const navbar = document.getElementById('moveNav'); // 获取 moveNav 元素
-    const navLinks = document.querySelectorAll('.navigation-bar a'); // 获取导航栏中的所有链接
-    const rightNavLinks = document.querySelectorAll('.right-nav a'); // 获取右侧导航栏中的所有链接
-    const rightNavIcons = document.querySelectorAll('.right-nav i'); // 获取右侧导航栏中的所有图标
+    const navLinks = document.querySelectorAll('.navigation-bar a'); 
+    const rightNavLinks = document.querySelectorAll('.right-nav a'); 
+    const rightNavIcons = document.querySelectorAll('.right-nav i'); 
 
     // 监听滚动事件
     window.addEventListener('scroll', function() {
         if (window.scrollY > 1) {  // 页面滚动超过1px时
-            navbar.classList.add('scrolled'); // 添加 scrolled 类
-            // 保持悬停效果，改变导航栏文字和图标颜色
+            navbar.classList.add('scrolled'); 
+           
             document.body.classList.add('scroll-active');
         } else {
             navbar.classList.remove('scrolled'); // 页面回到最上方时移除类
